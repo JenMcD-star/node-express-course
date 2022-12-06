5d43634467539d907cd19f0a672424a2dd92c133
JenMcD-star <jenniferannvancura@gmail.com>
2022-12-06 12:17:35 -0600

    week 3 assignment

--- 02-express-tutorial/07-params.js
+++ 02-express-tutorial/07-params.js
@@ -32,12 +32,27 @@ app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
   console.log(req.params)
 res.send('hello')
 })
-
 app.get('/api/v1/query', (req, res) => {
-  console.log(req.query)
-  res.send('hello again')
+  // console.log(req.query)
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  if (sortedProducts.length < 1) {
+    // res.status(200).send('no products matched your search');
+    return res.status(200).json({ sucess: true, data: [] })
+  }
+  res.status(200).json(sortedProducts)
 })
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000....");
-});
+  console.log('Server is listening on port 5000....')
+})
+
