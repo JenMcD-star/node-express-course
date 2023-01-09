7c8d39acadb97f03534cd00a8cf86d9c6791ec79
JenMcD-star <jenniferannvancura@gmail.com>
2023-01-09 12:29:59 -0600

    Display the hello user message on the front end.

--- 05-JWT-Basics/Preferred/public/login.js
+++ 05-JWT-Basics/Preferred/public/login.js
@@ -1,7 +1,37 @@
-const usernameInput = document.getElementById("username");
-const userPassword = document.getElementById("password");
-const submitBtn = document.getElementById("button");
+const usernameInputDOM = document.getElementById("username");
+const passwordInputDOM = document.getElementById("password");
+const submitBtn = document.getElementById("submitButton");
+const displayBtn = document.getElementById("displayBtn");
+const display = document.getElementById("display");
 
-submitBtn.addEventListener("click", () => {
-    alert('clicked')
-})
+submitBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const username = usernameInputDOM.value;
+  const password = passwordInputDOM.value;
+
+  try {
+    const { data } = await axios.post("/api/v1/logon", { username, password });
+    localStorage.setItem("token", data.token);
+    console.log("got it");
+  } catch (error) {
+    console.log(error);
+    localStorage.removeItem("token");
+  }
+});
+
+displayBtn.addEventListener ("click", async (e) => {
+    const token = localStorage.getItem('token')
+    try {
+      const { data } = await axios.get('/api/v1/hello', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      display.innerHTML = `<h5>${data.msg}</h5>`
+  
+    } catch (error) {
+      localStorage.removeItem('token')
+      display.innerHTML = `<p>${error.response.data.msg}</p>`
+    }
+  })
+  
